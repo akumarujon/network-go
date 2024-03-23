918e31dd1a24fd08f3e845543b78e877f2f8e95d
akumarujon <isakulovdev@gmail.com>
2024-03-23 17:14:04 +0500

    (feat): use 4 cores

--- main.go
+++ main.go
@@ -4,11 +4,24 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"interview/database"
 	"interview/routes"
+	"log/slog"
+	"os"
+	"runtime"
+	"strconv"
 )
 
 func main() {
+	numCPU := runtime.NumCPU()
+	err := os.Setenv("GOMAXPROCS", strconv.Itoa(numCPU))
+	if err != nil {
+		slog.Error("Failed to set GOMAXPROCS: ", err)
+	}
+
 	database.Migrate()
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		Prefork: true,
+		AppName: "Network Server",
+	})
 	routes.Setup(app)
 
 	err := app.Listen(":3000")
